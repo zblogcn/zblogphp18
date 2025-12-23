f0a90b9b44961df04ce06dbda0ca610938bff706
zx.asd <zhuzhuxf@gmail.com>
2025-12-23 13:48:15 +0800

    修改zbp_admin2_RegisterBackEndApp;

--- zb_system/admin2/function/admin2_function.php
+++ zb_system/admin2/function/admin2_function.php
@@ -7,26 +7,21 @@ require __DIR__ . "/admin2_misc.php";
 
 $zbp->ismanage = true;
 
-
-if (is_readable($xmlPath = $zbp->systemdir . 'admin2/backend-legacy/backend.xml')) {
-  zbp_admin2_RegisterBackEndApp('backend-legacy', $xmlPath);
-}
-if (is_readable($xmlPath = $zbp->systemdir . 'admin2/backend-nexus/backend.xml')) {
-  zbp_admin2_RegisterBackEndApp('backend-nexus', $xmlPath);
-}
-if (is_readable($xmlPath = $zbp->systemdir . 'admin2/backend-toyean/backend.xml')) {
-  zbp_admin2_RegisterBackEndApp('backend-toyean', $xmlPath);
-}
-unset($xmlPath);
+zbp_admin2_RegisterBackEndApp('backend-legacy', $zbp->systemdir . 'admin2/backend-legacy/backend.xml');
+zbp_admin2_RegisterBackEndApp('backend-nexus', $zbp->systemdir . 'admin2/backend-nexus/backend.xml');
+zbp_admin2_RegisterBackEndApp('backend-toyean', $zbp->systemdir . 'admin2/backend-toyean/backend.xml');
 
 // admin2 注册后台主题
 function zbp_admin2_RegisterBackEndApp($app_id, $app_file)
 {
   global $zbp;
   $app = new App();
-  $app->LoadInfoByXml('backend', $app_id, $app_file);
-  if ($app->isloaded == true) {
-    $zbp->backend_apps[$app_id] = $app;
+  if (is_readable($app_file)) {
+    $app->LoadInfoByXml('backend', $app_id, $app_file);
+    if ($app->isloaded == true) {
+      $zbp->backend_apps[$app_id] = $app;
+      return true;
+    }
   }
 }
 
