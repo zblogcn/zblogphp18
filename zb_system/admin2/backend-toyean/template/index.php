ae8b4f690b81dbd0844947d5469b5bd0ab73feb4
zx.asd <zhuzhuxf@gmail.com>
2025-12-28 16:48:03 +0800

    把默认后台模板复制一次;

--- zb_system/admin2/backend-toyean/template/index.php
+++ zb_system/admin2/backend-toyean/template/index.php
@@ -13,24 +13,28 @@
   <meta charset="utf-8" />
   <meta name="generator" content="{$zblogphp}" />
   <title>{$name} - {$title}</title>
-    <link rel="stylesheet" href="{$host}zb_system/admin2/{$backendtheme}/style/style.css?v={$version}">
-  <link rel="stylesheet" href="{$host}zb_system/image/icon/icon.css?v={$version}">
+  <link rel="stylesheet" href="{$host}zb_system/admin2/{$backendtheme}/style/style.css?v={$version}">
+  <!--<link rel="stylesheet" href="{$host}zb_system/image/icon/icon.css?v={$version}">-->
   <script src="{$host}zb_system/script/jquery-2.2.4.min.js?v={$version}"></script>
-  <script src="{$host}zb_system/script/jquery-ui.custom.min.js?v={$version}"></script>
+  <!--<script src="{$host}zb_system/script/jquery-ui.custom.min.js?v={$version}"></script>-->
+  <script src="{$host}zb_system/admin2/{$backendtheme}/script/common.js"></script>
   <script src="{$host}zb_system/script/zblogphp.js?v={$version}"></script>
   <script src="{$host}zb_system/script/c_admin_js_add.php?v={$version}"></script>
   {$header}
 {php}HookFilterPlugin('Filter_Plugin_Admin_Header');{/php}
 </head>
-
 <body class="admin admin-{$action}">
+  <div class="wrapper">
   <!-- <p>title: {$title}</p> -->
   <!-- <p>action: {$action}</p> -->
-  {template:layout_top}
   {template:layout_left}
+  <div class="main">
+  {template:layout_top}
   {template:layout_main}
+  </div>
   {$footer}
 {php}HookFilterPlugin('Filter_Plugin_Admin_Footer');{/php}
+  </div>
 </body>
 
 </html>
