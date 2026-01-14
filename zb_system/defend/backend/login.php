b520b3ccd3ed9166150987bbbbc038a9a827feae
wdssmq <wdssmq@qq.com>
2026-01-14 23:45:27 +0800

    后台模板-旧：重构登录页面样式，优化表单布局和组件引入

--- zb_system/defend/backend/login.php
+++ zb_system/defend/backend/login.php
@@ -17,43 +17,52 @@
     </script> -->
     <!-- <script src="{$host}zb_system/admin2/script/c_admin_js_add.js?v={$version}"></script> -->
     <!-- <script src="{$host}zb_system/admin2/{$backendtheme}/script/{$backendtheme}.js?v={$version}"></script> -->
-    <link rel="stylesheet" href="{$host}zb_system/css/admin.css">
+    <!-- <link rel="stylesheet" href="{$host}zb_system/css/admin.css"> -->
     {$header}
     {php}HookFilterPlugin('Filter_Plugin_Login_Header');{/php}
 </head>
 
-<body class="login admin-login">
-    <div class="bg">
-        <div id="wrapper">
-            <div class="logo"><img src="{$host}zb_system/image/admin/none.gif" title="{htmlspecialchars($blogname)}" alt="{htmlspecialchars($blogname)}" /></div>
-            <div class="login">
-                <form method="post" action="#">
-                    <input type="hidden" name="csrfToken" value="{$zbp.GetCSRFToken('login','minute')}">
-                    <dl>
-                        <dt></dt>
-                        <dd class="username"><label for="edtUserName">{$lang['msg']['username']}</label><input type="text" id="edtUserName" name="edtUserName" size="20" value="{php}echo GetVars('username', 'COOKIE');{/php}" tabindex="1" /></dd>
+<body class="admin-login">
+    <header class="login-header">
+        <h1 class="brand"><img src="{$host}zb_system/image/admin/none.gif" title="{htmlspecialchars($blogname)}" alt="{htmlspecialchars($blogname)}" /></h1>
+    </header>
 
-                        <dd class="password"><label for="edtPassWord">{$lang['msg']['password']}</label><input type="password" id="edtPassWord" name="edtPassWord" size="20" tabindex="2" /></dd>
 
-                        {if $zbp->option['ZC_LOGIN_VERIFY_ENABLE']}
-                        <dd class="validcode"><label for="edtValidcode">{$lang['msg']['validcode']}</label><input type="text" maxlength="{$zbp.option['ZC_VERIFYCODE_LENGTH']}" id="edtValidcode" name="verify" size="20" tabindex="10" />
-                            <img src="{$host}zb_system/script/c_validcode.php?id=login&time=m" onClick="javascript:this.src='{$host}zb_system/script/c_validcode.php?id=login&time=m&tm='+Math.random();" alt="validcode" />
-                        </dd>
-                        {/if}
-
-                    </dl>
-                    <dl>
-                        <dt></dt>
-                        <dd class="checkbox"><input type="checkbox" name="chkRemember" id="chkRemember" tabindex="98" /><label for="chkRemember">{$lang['msg']['stay_signed_in']}</label></dd>
-                        <dd class="submit"><input id="btnPost" name="btnPost" type="submit" value="{$lang['msg']['login']}" class="button" tabindex="99" /></dd>
-                    </dl>
-                    <input type="hidden" name="username" id="username" value="" />
-                    <input type="hidden" name="password" id="password" value="" />
-                    <input type="hidden" name="savedate" id="savedate" value="1" />
-                </form>
+    <main class="login-container">
+        <form class="login-form" method="post" action="#">
+            <input type="hidden" name="csrfToken" value="{$zbp.GetCSRFToken('login','minute')}">
+            <div class="form-row two-col">
+                <div class="field username">
+                    <label for="edtUserName">{$lang['msg']['username']}</label>
+                    <input type="text" id="edtUserName" name="edtUserName" size="20" value="{php}echo GetVars('username', 'COOKIE');{/php}" tabindex="1" />
+                </div>
+                <div class="field password">
+                    <label for="edtPassWord">{$lang['msg']['password']}</label>
+                    <input type="password" id="edtPassWord" name="edtPassWord" size="20" tabindex="2" />
+                </div>
             </div>
-        </div>
-    </div>
+
+            {if $zbp->option['ZC_LOGIN_VERIFY_ENABLE']}
+            <div class="form-row captcha">
+                <div class="field">
+                    <label for="edtValidcode">{$lang['msg']['validcode']}</label>
+                    <input type="text" maxlength="{$zbp.option['ZC_VERIFYCODE_LENGTH']}" id="edtValidcode" name="verify" size="20" tabindex="10" />
+                    <img class="captcha-img" src="{$host}zb_system/script/c_validcode.php?id=login&time=m" onClick="javascript:this.src='{$host}zb_system/script/c_validcode.php?id=login&time=m&tm='+Math.random();" alt="validcode" />
+                </div>
+                <div class="field placeholder">
+                </div>
+            </div>
+            {/if}
+
+            <div class="form-row actions">
+                <label class="remember"><input type="checkbox" name="chkRemember" id="chkRemember" tabindex="98" />{$lang['msg']['stay_signed_in']}</label>
+                <input id="btnPost" name="btnPost" type="submit" value="{$lang['msg']['login']}" class="button" tabindex="99" />
+            </div>
+            <input type="hidden" name="username" id="username" value="" />
+            <input type="hidden" name="password" id="password" value="" />
+            <input type="hidden" name="savedate" id="savedate" value="1" />
+        </form>
+    </main>
     <script>
         $("#btnPost").click(function() {
             var strUserName = $("#edtUserName").val();
