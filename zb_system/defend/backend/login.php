336e5396adfe9d29f87320aa5a3c7101ff69b984
zx.asd <zhuzhuxf@gmail.com>
2026-01-16 20:42:58 +0800

    修改login.php模板为原先元素和排列;修改css并编译;

--- zb_system/defend/backend/login.php
+++ zb_system/defend/backend/login.php
@@ -31,31 +31,30 @@
     <main class="login-container">
         <form class="login-form" method="post" action="#">
             <input type="hidden" name="csrfToken" value="{$zbp.GetCSRFToken('login','minute')}">
-            <div class="form-row">
-                <div class="field username">
+            <dl>
+                <dd class="username">
                     <label for="edtUserName">{$lang['msg']['username']}</label>
                     <input type="text" id="edtUserName" name="edtUserName" size="20" value="{php}echo GetVars('username', 'COOKIE');{/php}" tabindex="1" />
-                </div>
+                </dd>
 
-                <div class="field password">
+                <dd class="password">
                     <label for="edtPassWord">{$lang['msg']['password']}</label>
                     <input type="password" id="edtPassWord" name="edtPassWord" size="20" tabindex="2" />
-                </div>
+                </dd>
 
                 {if $zbp->option['ZC_LOGIN_VERIFY_ENABLE']}
-                <div class="field validcode">
+                <dd class="validcode">
                     <label for="edtValidcode">{$lang['msg']['validcode']}</label>
                     <input type="text" maxlength="{$zbp.option['ZC_VERIFYCODE_LENGTH']}" id="edtValidcode" name="verify" size="20" tabindex="10" />
                     <img class="captcha-img" src="{$host}zb_system/script/c_validcode.php?id=login&time=m" onClick="javascript:this.src='{$host}zb_system/script/c_validcode.php?id=login&time=m&tm='+Math.random();" alt="validcode" />
-                </div>
-                <div class="field placeholder"></div>
+                </dd>
                 {/if}
-            </div>
+            </dl>
 
-            <div class="form-row actions">
-                <label class="remember"><input type="checkbox" name="chkRemember" id="chkRemember" tabindex="98" />{$lang['msg']['stay_signed_in']}</label>
-                <input id="btnPost" name="btnPost" type="submit" value="{$lang['msg']['login']}" class="button" tabindex="99" />
-            </div>
+            <dl>
+                <dd class="checkbox"><input type="checkbox" name="chkRemember" id="chkRemember"  tabindex="98" /><label for="chkRemember">{$lang['msg']['stay_signed_in']}</label></dd>
+                <dd class="submit"><input id="btnPost" name="btnPost" type="submit" value="{$lang['msg']['login']}" class="button" tabindex="99"/></dd>
+            </dl>
             <input type="hidden" name="username" id="username" value="" />
             <input type="hidden" name="password" id="password" value="" />
             <input type="hidden" name="savedate" id="savedate" value="1" />
