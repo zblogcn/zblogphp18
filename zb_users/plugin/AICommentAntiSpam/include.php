07c16bb0077c07031fa105b782ef6554c379607e
GitHub Action <action@github.com>
2026-01-30 10:15:12 +0000

    Auto Commit - PhpCsFixer

--- zb_users/plugin/AICommentAntiSpam/include.php
+++ zb_users/plugin/AICommentAntiSpam/include.php
@@ -1,9 +1,11 @@
 <?php
 
 if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
-    define('AICommentAntiSpam_Version',1.2);
+    define('AICommentAntiSpam_Version', 1.2);
+
     require_once dirname(__FILE__) . '/vendor/autoload.php';
+
     require_once dirname(__FILE__) . '/includes/plugin_function.php';
 }
 
-RegisterPlugin("AICommentAntiSpam","ActivePlugin_AICommentAntiSpam");
+RegisterPlugin('AICommentAntiSpam', 'ActivePlugin_AICommentAntiSpam');
