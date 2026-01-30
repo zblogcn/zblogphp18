ba61b35f87e0d2819bbac400d182227067a1a586
zx.asd <zhuzhuxf@gmail.com>
2026-01-30 18:14:56 +0800

    改AntiSpam;

--- zb_users/plugin/AICommentAntiSpam/include.php
+++ zb_users/plugin/AICommentAntiSpam/include.php
@@ -1,8 +1,9 @@
 <?php
 
-define('AICommentAntiSpam_Version',1.2);
-require_once dirname(__FILE__) . '/vendor/autoload.php';
-require_once dirname(__FILE__) . '/includes/plugin_function.php';
-
+if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
+    define('AICommentAntiSpam_Version',1.2);
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+    require_once dirname(__FILE__) . '/includes/plugin_function.php';
+}
 
 RegisterPlugin("AICommentAntiSpam","ActivePlugin_AICommentAntiSpam");
