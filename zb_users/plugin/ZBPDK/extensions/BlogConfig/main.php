92d8b47321cb04ad7b3e57d2c1b3d1a8ddf9d585
zx.asd <zhuzhuxf@gmail.com>
2026-02-27 00:12:51 +0800

    将ZBPDK升级到1.8后台，好麻烦，只做了一部分！！！;

--- zb_users/plugin/ZBPDK/extensions/BlogConfig/main.php
+++ zb_users/plugin/ZBPDK/extensions/BlogConfig/main.php
@@ -1,6 +1,9 @@
 <?php
 require '../../../../../zb_system/function/c_system_base.php';
 require '../../../../../zb_system/function/c_system_admin.php';
+if (version_compare(ZC_VERSION, '1.8.0') >= 0) {
+    require '../../../../../zb_system/admin2/function/admin2_function.php';
+}
 require '../../zbpdk_include.php';
 header("Cache-Control: no-cache, must-revalidate");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
@@ -20,6 +23,8 @@ if (!$zbp->CheckPlugin('ZBPDK')) {
     $zbp->ShowError(48);
     die();
 }
+
+
 
 if (isset($_GET['act'])) {
     if (function_exists('CheckHTTPRefererValid') && !CheckHTTPRefererValid()) {
@@ -83,54 +88,10 @@ if (isset($_POST['act'])) {
     }
 }
 
-require $blogpath . 'zb_system/admin/admin_header.php';
-?>
-<link rel="stylesheet" href="BlogConfig.css" type="text/css" media="screen"/>
-<link rel="stylesheet" href="../../css/jquery.contextMenu.css" type="text/css" media="screen"/>
-<script type="text/javascript" src="../../js/jquery.contextMenu.js"></script>
-<script type="text/javascript" src="BlogConfig.js"></script>
-<script src="../../../../../zb_system/script/c_admin_js_add.php" type="text/javascript"></script>
-<script src="../../../../../zb_system/script/jquery-ui.custom.min.js" type="text/javascript"></script>
-<?php
-require $blogpath . 'zb_system/admin/admin_top.php';
-?>
 
-<div id="divMain">
-  <div class="divHeader"><?php echo $blogtitle; ?></div>
-  <div class="SubMenu"><?php echo $zbpdk->submenu->export('BlogConfig'); ?></div>
-  <div id="divMain2">
-    <div class="DIVBlogConfig">
-      <div class="DIVBlogConfignav" name="tree" id="tree">
-        <ul>
-            <?php echo blogconfig_left(); ?>
-        </ul>
-        <script type="text/javascript">
-        $(document).ready(function() {
-            $.contextMenu({
-                selector: '#tree ul li', 
-                items: {
-                    "open": {name: "打开"},
-                    "rename": {name: "重命名"},
-                    "del": {name: "删除"}
-                }, 
-                callback: function (key, options) {
-//					console.log(this);
-                    run(key, $(this).find("a").attr("id"));
-                }
-            });
-        });
-      </script></div>
-      <div id="content" class="DIVBlogConfigcontent">
-        <div class="DIVBlogConfigcontentbody">请选择</div>
-      </div>
-      <div class="clear"></div>
-    </div>
-  </div>
-</div>
 
-<script>ActiveTopMenu('zbpdk');</script>
-<script type="text/javascript">AddHeaderIcon("<?php echo $bloghost . 'zb_users/plugin/ZBPDK/logo.png'; ?>");</script>
-<?php
-require $blogpath . 'zb_system/admin/admin_footer.php';
-RunTime();
-?>
+if (version_compare(ZC_VERSION, '1.8.0') >= 0) {
+    require 'main2.php';
+} else {
+    require 'main1.php';
+}
---
+++ zb_users/plugin/ZBPDK/extensions/BlogConfig/main2.php
@@ -0,0 +1,57 @@
+<?php
+
+$blogtitle = 'Z-Blog PHP Development';
+
+$ActionInfo = zbp_admin2_GetActionInfo($action, (object) [
+    'Title' => $blogtitle,
+    'Header' => $blogtitle,
+    'HeaderIcon' => $bloghost . 'zb_users/plugin/ZBPDK/logo.png',
+    'Content' => Get_Content(),
+    'SubMenu' => $zbpdk->submenu->export('BlogConfig'),
+    'ActiveTopMenu' => 'zbpdk',
+    'HtmlHeader' => '<style>#divMain2{padding-top:1em!important ;}</style><link rel="stylesheet" href="BlogConfig.css" type="text/css" media="screen"/>
+<link rel="stylesheet" href="../../css/jquery.contextMenu.css" type="text/css" media="screen"/>
+<script type="text/javascript" src="../../js/jquery.contextMenu.js"></script>
+<script type="text/javascript" src="BlogConfig.js"></script>',
+]);
+
+// 输出页面
+$zbp->template_admin->SetTags('title', $ActionInfo->Title);
+$zbp->template_admin->SetTags('main', $ActionInfo);
+$zbp->template_admin->Display('index');
+
+function Get_Content()
+{
+  global $zbpdk, $zbp;
+    ob_start(); ?>
+    <div class="DIVBlogConfig">
+      <div class="DIVBlogConfignav" name="tree" id="tree">
+        <ul>
+            <?php echo blogconfig_left(); ?>
+        </ul>
+        <script type="text/javascript">
+        $(document).ready(function() {
+            $.contextMenu({
+                selector: '#tree ul li', 
+                items: {
+                    "open": {name: "打开"},
+                    "rename": {name: "重命名"},
+                    "del": {name: "删除"}
+                }, 
+                callback: function (key, options) {
+//                  console.log(this);
+                    run(key, $(this).find("a").attr("id"));
+                }
+            });
+        });
+      </script></div>
+      <div id="content" class="DIVBlogConfigcontent">
+        <div class="DIVBlogConfigcontentbody">请选择</div>
+      </div>
+      <div class="clear"></div>
+    </div>
+<?php
+    $content = ob_get_clean();
+
+    return $content;
+}
